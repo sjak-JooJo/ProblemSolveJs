5065c26412d00bea891a7bf91bd9ef49ba9fca88
JooJo <sjak0101@gmail.com>
2022-10-20 00:53:19 +0100

    error skip from array

--- problemSolve.js
+++ problemSolve.js
@@ -4,7 +4,7 @@ function getHigherAndLowerValue(arr){
     let high = arr[0]; 
     let low = arr[0]; 
     for(let i = 0; i < arr.length; i++){
-       // if(arr[i].typeof !== "number") continue;
+       if(typeof arr[i] !== "number") continue;
 
         if(high > arr[i]){
             high = arr[i];
