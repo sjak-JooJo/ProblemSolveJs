16e1731f2001f3a64751bf32c8cd1c1e9fe68013
JooJo <sjak0101@gmail.com>
2022-11-16 02:24:05 +0000

    binary search using js

---
+++ binarySearch.js
@@ -0,0 +1,26 @@
+
+function binarySearch(arr, value){
+    let start = 0;
+    let end = arr.length - 1;
+    
+
+    
+    while(start <= end){
+        let middle = Math.round((start + end )/ 2);
+        if(arr[middle] === value) return middle;
+
+        if(value > arr[middle]){
+            start = middle + 1;
+        }else if ( value < arr[middle]){
+            end = middle - 1;
+        }
+    }
+    return -1;
+
+    //console.log(middle);
+
+}
+
+
+
+console.log(binarySearch([2, 3, 4, 7, 34, 66], 2))
